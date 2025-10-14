6c8071e71ddf58b630cca73e78bb69f889942f0e
Menezes <leyvisonmenezes@gmail.com>
2025-10-14 11:32:24 -0300

    tratamento de erros

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express()
 
-//Diz ao Express para interpretar automaticamente o corpo (body) das requisições HTTP no formato JSON.
+//Diz ao Express para interpretar automaticamente o body das requisições HTTP no formato JSON.
 app.use(express.json())
 
 export { app }
---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/app-error";
+import { ZodError, z } from "zod";
+
+export function ErrorHandling(error: any, request: Request, response: Response, next: NextFunction){
+    if(error instanceof AppError){
+        //Caso ocorra algo inesperado, a app para, e lança uma excessão nos códigos pré-definidos, conforme o APPError
+        return response.status(error.statusCode).json({ message: error.message })
+    }
+    if(error instanceof ZodError){
+        //Para Erros de Validação
+        return response.status(400).json({ message: "Validation Error", issues: z.treeifyError(error) })
+    }
+    return response.status(500).json({ message: error.message })
+}
+
